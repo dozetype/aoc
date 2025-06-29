eb445bd6dfcbd64f8e39a54f8d4ede065f53b45e
xw <johntheghostie@gmail.com>
2025-06-29 20:40:45 +0800

    cpp d1
    - Creates CMakeLists.txt for building executable files.
    - Adds .gitignore to exclude unwanted files.
    - Adds initial cpp file with example data parsing.

---
+++ cpp/d1.cpp
@@ -0,0 +1,49 @@
+#include <assert.h>
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <string>
+
+int main() {
+    std::ifstream file("puzzles/d1.txt");
+    assert(file.is_open());
+
+    std::string line;
+    std::string token;
+    std::vector<int> left;
+    std::vector<int> right;
+    std::unordered_map<int, int> similarity;
+    std::regex delim("\\s"); //delimit all space similar to r"\s"g in python
+    while (std::getline(file, line)) {
+        std::sregex_token_iterator it(
+            line.begin(),
+            line.end(),
+            delim,
+            -1 // special value: return the parts *between* matches. 0 for the matches
+        );
+        std::sregex_token_iterator end;
+        std::vector<int> temp;
+        for (; it != end; ++it) {
+            if (!it->str().empty()) {
+                temp.push_back(stoi(it->str()));
+            }
+        }
+        left.push_back(temp[0]);
+        right.push_back(temp[1]);
+        if (!similarity.contains(temp[1])) {
+            similarity[temp[1]] = 1;
+        } else {
+            similarity[temp[1]]++;
+        }
+    }
+    std::ranges::sort(left);
+    std::ranges::sort(right);
+    int ans{};
+    int ans2{};
+    for (int i = 0; i < left.size(); i++) {
+        ans += abs(left[i] - right[i]);
+        ans2 += similarity[left[i]] * left[i];
+    }
+    std::cout << ans << "\n" << ans2 << std::endl;
+    return 0;
+}
