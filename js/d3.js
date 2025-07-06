bd2e682a1a7006e488aba485ff8453eb6af68cc3
xw <johntheghostie@gmail.com>
2025-07-06 23:27:00 +0800

    d3 update

--- js/d3.js
+++ js/d3.js
@@ -2,22 +2,25 @@ const fs = require("fs");
 
 fs.readFile("puzzles/d3.txt", "utf-8", (lol, data) => {
     const lines = data.split(/\n|\r\n/);
-    let ans = 0;
+    let ans1 = 0;
+    let ans2 = 0;
     let enabled = true;
     lines.forEach((line) => {
         if (line == "") return;
         const res = line.match(/mul\(\d+,\d+\)|do\(\)|don\'t\(\)/g);
         res.forEach((mul) => {
-            // console.log(mul);
             if (mul == "do()") {
                 enabled = true;
             } else if (mul == "don't()") {
                 enabled = false;
-            } else if (enabled) {
+            } else {
                 const nums = mul.match(/\d+/g);
-                ans += nums[0] * nums[1];
+                ans1 += nums[0] * nums[1];
+                if (enabled) {
+                    ans2 += nums[0] * nums[1];
+                }
             }
         });
     });
-    console.log(ans);
+    console.log("part1:", ans1, " part2:", ans2);
 });
