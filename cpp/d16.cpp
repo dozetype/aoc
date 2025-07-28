de767e58f11663740ce7fd459e9d9cfc62ad6e37
xw <johntheghostie@gmail.com>
2025-07-29 00:15:42 +0800

    Update
    Using a Node to hold info of each position

--- cpp/d16.cpp
+++ cpp/d16.cpp
@@ -1,21 +1,37 @@
 #include "aoc.h"
 
+struct Node{
+    int dist{INT_MAX};
+    pair<int, int> prev;
+    bool wall;
+    
+    Node(bool isWall = false):wall(isWall){};
+};
+
 int main(){
     ifstream file("puzzles/d16.txt");
     string line;
-    vector<vector<char>> maze;
+    vector<vector<Node>> maze;
+    pair<int, int> startPos;
+    pair<int, int> endPos;
+    int x{}, y{};
     while(getline(file, line)){
+        x = 0;
         stringstream ss(line);
         char c;
-        vector<char> currLine;
+        vector<Node> currLine;
         while(ss >> c){
-            currLine.push_back(c);
+            if(c == 'S') startPos = {y, x};
+            else if(c == 'E') startPos = {y, x};
+            if(c != '#') currLine.emplace_back(Node(false));
+            else currLine.emplace_back(Node(true));
         }
         maze.push_back(currLine);
+        ++y;
     }
     for(auto i: maze){
         for(auto x: i){
-            cout << x;
+            cout << x.wall;
         }
         cout << "\n";
     }
